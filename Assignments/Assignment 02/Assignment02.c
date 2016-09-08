e905d2612e757339ab8c922828739a25ebe9e71f
ibera <ibera@DESKTOP-28PBUAT>
2016-09-08 12:09:19 -0400

    Finished assignment 2

---
+++ Assignments/Assignment 02/Assignment02.c
@@ -0,0 +1,26 @@
+/*
+*	Written by: Omar Rahman
+*	Date: 9/8/2016 @11:05am EST
+*	Purpose: Assignment 02
+*/
+
+#include <stdio.h>
+
+main() {
+
+	int num1;
+	int num2;
+
+	printf("Type in number 1: ");
+	scanf_s("%d", &num1);
+	
+	printf("Type in number 2: ");
+	scanf_s("%d", &num2);
+
+	printf("The sum of the two numbers is: %d", num1 + num2);
+
+	printf("\n");
+
+	system("pause");
+
+}
