a16ac2afff3d65624f156d1e1393ee592e4f24d7
IBeRamen <iberamen@gmail.com>
2016-09-20 11:09:25 -0400

    Finished part of assignment 04

--- Assignments/Assignment 04/Assignment04.c
+++ Assignments/Assignment 04/Assignment04.c
@@ -1,6 +1,31 @@
 /*
 *	Written by: Omar Rahman
-*	Date: 9/19/2016
+*	Date: 9/19/2016 @12:02PM EST
 *	Purpose: Assignment 04
 */
 
+#include <stdio.h>
+#define PAUSE system("pause");
+
+main() {
+
+	int weight, miles, cost;
+
+	printf("What is the package weight (in pounds)? ");
+	scanf_s("%i", &weight);
+
+	printf("How many miles is this package being shipped? ");
+	scanf_s("%i", &miles);
+
+	if (weight <= 15)
+		cost = 15;
+	else
+		weight % 15 * 0.5;
+		miles / 500 * 10;
+		cost = weight + miles;
+
+	printf("Your total cost is: $%i\n", cost);
+
+	PAUSE;
+
+}
