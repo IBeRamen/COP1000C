38f6c90a7a387ffda46bb73691c4baeeb49f81dc
IBeRamen <iberamen@gmail.com>
2016-09-22 10:02:28 -0400

    Finished Assignment 04

--- Assignments/Assignment 04/Assignment04.c
+++ Assignments/Assignment 04/Assignment04.c
@@ -9,22 +9,29 @@
 
 main() {
 
-	int weight, miles, cost;
-
+	// Weight is an int because you cannot use % (modulus) with double, float
+	int weight;
+	float miles, cost, milesCharge, overWeight;
+	
 	printf("What is the package weight (in pounds)? ");
 	scanf_s("%i", &weight);
 
 	printf("How many miles is this package being shipped? ");
-	scanf_s("%i", &miles);
+	scanf_s("%f", &miles);
+
+	// Calculations
+	// milesCharge gets the miles the user inputed and divides it by 500 then * it by 10 (So $10 / 500)
+	// overWeight gets the weight and finds the remainder of 15 to get the amount it's over 15
+	milesCharge = miles / 500 * 10;
+	overWeight = weight % 15;
 
 	if (weight <= 15)
-		cost = 15;
+		cost = 15 + milesCharge;
+	
 	else
-		weight % 15 * 0.5;
-		miles / 500 * 10;
-		cost = weight + miles;
+		cost = (overWeight * 0.5) + milesCharge + 15;
 
-	printf("Your total cost is: $%i\n", cost);
+	printf("Your total cost is: $%0.2f\n", cost);
 
 	PAUSE;
 
