700a61679c8c03df85a31707faa451d6766dfdc3
IBeRamen <iberamen@gmail.com>
2016-09-29 22:33:22 -0400

    Changed logic

--- Assignments/Assignment 06/Assignment06.c
+++ Assignments/Assignment 06/Assignment06.c
@@ -1,47 +1,49 @@
-/*
-*	Written by: Omar Rahman
-*	Date: 9/28/2016 @10:06AM EST
-*	Purpose: Assignment 06
-*/
+/* * Written by: Omar Rahman 
+ * * Date: 9/28/2016 @10:06AM EST 
+ * * Purpose: Assignment 06 */ 
 
-#include <stdio.h>
+#include <stdio.h> 
+#define PAUSE system("pause"); 
 
-#define PAUSE system("pause");
-
-main() {
-
-	float week1, week2, week3, avgSpent = 0.0;
-
-	printf("How much have you spent on groceries in Week 1?: ");
-	scanf_s("%f", &week1);
-
-	printf("How much have you spent on groceries in Week 2?: ");
-	scanf_s("%f", &week2);
-
-	printf("How much have you spent on groceries in Week 3?: ");
+int main()
+{
+	float week1, week2, week3, avgSpent, totalSpent = 0.0; 
+	
+	printf("How much have you spent on groceries in Week 1?: "); 
+	scanf_s("%f", &week1); 
+	printf("How much have you spent on groceries in Week 2?: "); 
+	scanf_s("%f", &week2); 
+	printf("How much have you spent on groceries in Week 3?: "); 
 	scanf_s("%f", &week3);
 
-	avgSpent = (week1 + week2 + week3) / 3;
-
-	if (week1 < 50 || week2 < 50 || week3 < 50) {
+	totalSpent = week1 + week2 + week3;
+	avgSpent = totalSpent / 3; 
+	
+	if (week1 < 50 || week2 < 50 || week3 < 50)
+	{
+		printf("You spent: $%.2f\n", totalSpent);
 		printf("You must be starving!\n");
 	}
-	else if (week1 > 50 || week2 > 50 || week3 > 50 && week1 < 200 || week2 < 200 || week3 < 200) {
-		printf("You are eating well!\n");
+	else if (week1 >= 50 && week1 < 200 || week2 >= 50 && week2 < 200 || week3 >= 50 && week3 < 200)
+	{
+		printf("You are eating well!");
 	}
-	else {
-		printf("You spent: $%.2f\n", week1 + week2 + week3);
-		printf("You are going to get fat.\n");
+	else if (week1 > 200 || week2 > 200 || week3 > 200)
+	{
+		printf("You spent: $%.2f\n", totalSpent);
+		printf("You are going to get fat!\n");
 	}
 
-	if (week2 > week1) {
+	if (week2 > week1)
+	{
 		printf("Are you eating more?\n");
 	}
-	else if (week3 < week1 && week3 < week2) {
+	else if (week3 < week1 && week3 < week2)
+	{
 		printf("Are you on a diet?\n");
 	}
 
-	printf("Average amount you spent on groceries: $%.2f\n", avgSpent);
+	printf("Average spent: $%.2f\n", avgSpent);
 
 	PAUSE;
 
