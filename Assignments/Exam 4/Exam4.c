f9c80acd49ad82bc56cd1d848dfea4dab3762354
IBeRamen <iberamen@gmail.com>
2016-12-05 01:25:27 -0500

    Progress

--- Assignments/Exam 4/Exam4.c
+++ Assignments/Exam 4/Exam4.c
@@ -1,28 +1,74 @@
 /*
- * Project Name: Exam 4
- * Author: Omar Rahman
- * Date: 12/4/2016
- */
+* Project Name: Exam 4
+* Author: Omar Rahman
+* Date: 12/4/2016
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define PAUSE system("pause");
 #define CLS system("cls");
 #define FLUSH flush();
-#define MAXELEMENTS 256
+#define MAXELEMENTS 20
 
 void displayMenu();
 void flush();
-
 int getSelection();
 
-void displayArray(int a[], char m[]);
-char doTheSwitch(int a[], int i, char switchMade);
+void enterKeys();
+
 
 int main()
 {
 	int userSelection;
+
+	// Declare parallel arrays
+	int keyboardCodes[] = {
+		32, 33, 34, 35, 36, 37,
+		38, 39, 40, 41, 42, 43, 44, 45,
+		46, 47, 48, 49, 50, 51, 52, 53,
+		54, 55, 56, 57, 58, 59, 60, 61,
+		62, 63, 64, 65, 66, 67, 68, 69,
+		70, 71, 72, 73, 74, 75, 76, 77,
+		78, 79, 80, 81, 82, 83, 84, 85,
+		86, 87, 88, 89, 90, 91, 92, 93,
+		94, 95, 96, 97, 98, 99, 100, 101,
+		102, 103, 104, 105, 106, 107, 108,
+		109, 110, 111, 112, 113, 114, 115,
+		116, 117, 118, 119, 120, 121, 122
+	};
+	char *keyboardWords[] = {
+		"spacebar",
+		"!", "\"", "#", "$", "%", "&", "'",
+		"(", ")", "*", "+", ",", "-", ".",
+		"/", "0", "1", "2", "3", "4", "5",
+		"6", "7", "8", "9", ":", ";", "<",
+		"=", ">", "?", "@", "A", "B", "C",
+		"D", "E", "F", "G", "H", "I", "J",
+		"K", "L", "M", "N", "O", "P", "Q",
+		"R", "S", "T", "U", "V", "W", "X",
+		"Y", "Z", "[", "\\","]", "^", "_",
+		"`", "a", "b", "c", "d", "e", "f",
+		"g", "h", "i", "j", "k", "l", "m",
+		"n", "o", "p", "q", "r", "s", "t",
+		"u", "v", "w", "x", "y", "z"
+	};
+	int parrCodes[] = {
+		0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+		11, 12, 13, 14, 15, 16, 17, 18, 19,
+		20, 21, 22, 23, 24, 25, 26, 27, 28,
+		29, 30, 31, 32, 33, 34, 35, 36, 37,
+		38, 39, 40, 41, 42, 43, 44, 45, 46,
+		47, 48, 49, 50, 51, 52, 53, 54, 55,
+		56, 57, 58, 59, 60, 61, 62, 63, 64,
+		65, 66, 67, 68, 69, 70, 71, 72, 73,
+		74, 75, 76, 77, 78, 79, 80, 81, 82,
+		83, 84, 85, 86, 87, 88, 89, 90, 91
+	};
+
+	char keysPressed[1000] = { 0 };
 
 	do
 	{
@@ -34,30 +80,8 @@ int main()
 		case 1:
 			CLS;
 
-			int entered_keys[256] = { 0 };
-			int requested_keys = 0;
-			int i, bottom, j;
-			char switchMade;
-
-			bottom = MAXELEMENTS - 1;
-
-			printf("How many keys do you want to enter?");
-			scanf_s("%i", &requested_keys);
-
-			for (i = 0; i < requested_keys; i++)
-			{
-				printf("Enter a value: ");
-				scanf_s("%d", (entered_keys + i));
-				FLUSH;
-			}
-
-			/*
-			 * printf("\nEntered values:\n");
-			for (i = 0; i < requested_keys; i++)
-			{
-				printf("%d\n", *(entered_keys + i));
-			}
-			 */
+			printf("Press some key(s)");
+			scanf_s("%s", keysPressed);
 
 			PAUSE;
 
@@ -66,19 +90,6 @@ int main()
 		case 2:
 			CLS;
 
-			do {
-				switchMade = 'N';
-
-				for (j = 0; j < bottom; j++)
-				{
-					if (entered_keys[j] < entered_keys[j + 1]) {
-						switchMade = doTheSwitch(entered_keys, j, switchMade);
-					}
-				}
-				bottom--;
-			} while (switchMade == 'Y');
-
-			displayArray(entered_keys, "SORTED");
 
 			PAUSE;
 
@@ -101,6 +112,18 @@ int main()
 		case 5:
 			CLS;
 
+			for (int i = 0; i <= 90; i++) {
+				printf("Key: %s, Code: %d, Parallel: %s \n",
+					keyboardWords[i], keyboardCodes[i], keyboardWords[parrCodes[i]]);
+			}
+
+			PAUSE;
+
+			break;
+
+		case 6:
+			CLS;
+
 			break;
 
 		default:
@@ -109,7 +132,9 @@ int main()
 
 			break;
 		}
-	} while (userSelection != 5);
+	} while (userSelection != 6);
+
+	return 0;
 }
 
 void displayMenu()
@@ -122,7 +147,8 @@ void displayMenu()
 	printf("2. Display keys pressed (High to Low)\n");
 	printf("3. Display never pressed key(s)\n");
 	printf("4. Display most pressed key\n");
-	printf("5. Quit\n");
+	printf("5. Display parallel array\n");
+	printf("6. Quit\n");
 	printf("=======================================\n");
 
 	printf("Enter your selection: ");
@@ -142,32 +168,18 @@ int getSelection()
 	return result;
 } // end getSelection()
 
-char doTheSwitch(int array[], int i, char switchMade) {
-
-	char result = 'Y';
-
-	int temp;
-	temp = array[i];
-	array[i] = array[i + 1];
-	array[i + 1] = temp;
-
-	return result;
-}
-
-void displayArray(int a[], char m[]) {
-
-	int i;
-
-	printf("The array is %s.\n", m);
-
-	for (i = 0; i < MAXELEMENTS; i++) {
-		printf("Array[%i] contains %i\n", i, a[i]);
-	}
-
-	PAUSE;
-}
-
 void flush()
 {
 	while (getchar() != '\n');
 } // end flush();
+
+void enterKeys()
+{
+	char array[20];
+
+	fgets(array, 20, stdin);
+
+	array[strlen(array) - 1] = '\0';
+
+	printf("The user has entered '%s'\n", array);
+}
